a22bada2de1f16e5a4b4d79f82ab8b1c4d758e3d
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-02-12 23:30:55 +0100

    Test login and send email verify user

--- routes/auth.js
+++ routes/auth.js
@@ -13,12 +13,40 @@ router.post('/registro', function (req, res, next) {
       displayName: `${name} ${surname}`,
     })
     .then(data => {
-      res.status(200).json(data.toJSON())
+      let dataAux;
+      admin.generateSignInWithEmailLink(email).then(data => {
+        dataAux = data
+      }).catch(err => {
+        res.status(err.code).json(err.message).end()
+        console.log(err)
+      })
       console.log('Successfully updated user', data.toJSON());
+      res.status(200).json(data.toJSON()).end()
     })
     .catch(err => {
+      res.status(err.code).json(err.message)
       console.log(err);
     });
 });
+
+
+router.post('/login', function (req, res, next) {
+  const { email, password } = req.body;
+
+
+  admin.auth().signInWithEmailAndPassword(email, password)
+  .then((user) => {
+    console.log(user)
+    res.end()
+  })
+  .catch((error) => {
+    res.code(error.code).json(error.message)
+  });
+
+});
+
+
+
+
 
 module.exports = router;
