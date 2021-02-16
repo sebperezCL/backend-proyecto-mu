a336feb2f21925390110f51a47e9dff7312ad7f8
sebperez <sebperez@gmail.com>
2021-02-16 20:40:03 -0300

    agregada librería para conexión a mongodb, archivo .env

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const indexRouter = require("./routes/index");
-const auth = require("./routes/auth");
-const usersRouter = require("./routes/users");
+const indexRouter = require('./routes/index');
+const auth = require('./routes/auth');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+require('./lib/connectMongoose');
 
-app.use(logger("dev"));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use("/auth", auth);
-app.use("/users", usersRouter);
+app.use('/auth', auth);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -31,14 +32,14 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  console.log('handler', err)
+  console.log('handler', err);
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json({error: err });
+  res.json({ error: err });
 });
 
 module.exports = app;
