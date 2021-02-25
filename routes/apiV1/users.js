322535a895d478a304edf692b30ea4366bbece64
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-02-25 14:37:16 +0100

    update

--- routes/apiV1/users.js
+++ routes/apiV1/users.js
@@ -10,13 +10,12 @@ const userController = require('../../controllers/usersController');
 router.post(
   '/',
   [
-    body('uidFirebase', 'Indicate the user id').notEmpty(),
+    body('uid', 'Indicate the user id').notEmpty(),
     body('email', "Indicate the user's email address").notEmpty(),
-    body('names', 'Indicate the names of the user').notEmpty(),
+    body('displayName', 'Indicate the names of the user').notEmpty(),
     body('firstSurname', 'Indicate the first surname of the user').notEmpty(),
     body('secondSurname').isString(),
-    body('fiscalId', "Indicate the user's fiscal id").notEmpty(),
-    body('organization', "Indicate the user's organization").notEmpty()
+    body('fiscalNumber', "Indicate the user's fiscal id").notEmpty(),
   ],
   fieldsValidator,
   userController.createUser
