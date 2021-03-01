94c9e54787c2710486796a5e5522725206734b69
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-03-01 21:56:13 +0100

    Commit deploy script in dev

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
-  console.log(req.header())
+  console.log(req)
   
   res.end(200).end()
 })
