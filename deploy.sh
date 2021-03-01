310724bbeda4150b59bc19fd1645e7930e2ba132
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-03-01 19:10:05 +0100

    Commit deploy script in dev

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,18 @@
 
 #branch dev
 deploy () {
-  ssh MU-NODE 'sudo -u node -i' <<< 'cd node-mu && git checkout dev && git pull && pm2 restart www'
+  echo " _ _ " 
+  echo " _ _| |__ _ _ _ __ | |_ _ _ " 
+  echo "| | | | '_ \| | | | '_ \| __| | | |"
+  echo "| |_| | |_) | |_| | | | | |_| |_| |" 
+  echo " \__,_|_.__/ \__,_|_| |_|\__|\__,_|"
+
+  echo "Script de despliegue........." 
+  echo "este script añade todos los cambios commitea y pushea"
+  git add .
+  git commit -m "Commit deploy script in dev"
+  ssh MU-NODE 'sudo -u node -i' <<< 'cd node-mu && git checkout $1 && git pull && pm2 restart www'
 }
 
 deploy
+
