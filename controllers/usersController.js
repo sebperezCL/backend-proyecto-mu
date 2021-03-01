6f48bca09485423b51b166b2b5eed85e94b1fd53
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-03-01 21:27:02 +0100

    Commit deploy script in dev

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -75,8 +75,34 @@ async function disableUser(req, res, next) {
   }
 }
 
+
+/**
+ * Retorna un usuario
+ */
+async function getUser(req, res, next) {
+  try {
+    const usuario = await User.findOne({ email: req.body.uid });
+
+    if (!usuario)
+      return next(createError(404, 'El usuario NO existe en la base de datos'));
+
+    usuario.activo = true;
+    await usuario.save();
+
+    res
+      .status(200)
+      .json(formatoResponse('success', user, 'Usuario activado con éxito'));
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
+}
+
+
+
+
 module.exports = {
   createUser,
   enableUser,
   disableUser,
+  getUser
 };
