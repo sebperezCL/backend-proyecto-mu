48d6a4acf49ae01bf60b6b9a76019d9ccdb3ab7f
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-03-04 22:42:30 +0100

    Commit deploy script in dev

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const auth = require('./routes/apiV1/auth');
 const usersRouter = require('./routes/apiV1/users');
 const rolValidator = require('./lib/middlewares/rolValidator')
 const tokenDecode = require('./lib/middlewares/tokenDecode')
