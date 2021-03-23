8a3f462bbee253c3b2dcdb6ab9f4677b8b413369
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-03-23 23:53:35 +0100

    Commit deploy script in dev

---
+++ routes/apiV1/sendEmail.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const sender = require('../../lib/clientEmailSender/clientEmailSender');
+const formatoResponse = require('../../lib/formatoResponse')
+
+router.post('/', async (req, res, next) => {
+  try {
+
+    if (!(Object.keys(req.body).includes('email') && Object.keys(req.body).includes('type'))) {
+      res.statusCode(400).json(formatoResponse(400,'', 'Bad request', 'BADREQUEST'))
+    }
+
+    const { email, type, ...data } = req.body;
+
+    await sender({
+      to: email,
+      templateId: type,
+      dynamicTemplateData: data,
+    });
+    
+  } catch (err) {
+    next(err)
+    console.log('error');
+  }
+});
+
+module.exports = router;
