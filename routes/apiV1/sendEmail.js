789168f39747f1d2d03941542d03dbe16d94b1e1
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-03-24 23:15:24 +0100

    Commit deploy script in dev

--- routes/apiV1/sendEmail.js
+++ routes/apiV1/sendEmail.js
@@ -3,75 +3,93 @@ const router = express.Router();
 const createError = require('http-errors');
 
 const sender = require('../../lib/clientEmailSender/clientEmailSender');
-const formatoResponse = require('../../lib/formatoResponse')
-const User = require('../../models/User')
-
+const formatoResponse = require('../../lib/formatoResponse');
+const User = require('../../models/User');
 
 router.post('/', async (req, res, next) => {
   try {
-    
-    if (!(Object.keys(req.body).includes('email') && Object.keys(req.body).includes('type'))) {
-      return next(createError(400,'', 'Bad request', 'BADREQUEST'))
+    if (
+      !(
+        Object.keys(req.body).includes('email') &&
+        Object.keys(req.body).includes('type')
+      )
+    ) {
+      return next(createError(400, '', 'Bad request', 'BADREQUEST'));
     }
-    
-    const { email, type, ...data } = req.body;
-    console.log(req.body)
-    
-    await sender({
-      to: email,
-      templateId: process.env[type],
-      dynamicTemplateData: data,
-    });
+    console.log(req.body);
 
-    res.status(200).json(formatoResponse(200, 'ok', 'ok', ''))
-    
+    const { email, type, ...data } = req.body;
+
+    if (type === 'CONTACT') {
+      return await sender({
+        to: email,
+        templateId: process.env[type],
+        dynamicTemplateData: data,
+      });
+    }
+
+
+    if (type === 'INVOICE') {
+      await sender({
+        to: data.email,
+        templateId: process.env[type],
+        dynamicTemplateData: data,
+      });
+    }
+
+    res.status(200).json(formatoResponse(200, 'ok', 'ok', ''));
   } catch (err) {
-    next(err)
+    next(err);
     console.log('error');
   }
 });
 
-
 router.post('/treasurer-income', async (req, res, next) => {
   try {
-    
     let result;
 
-    if (!(Object.keys(req.body).includes('_id') && Object.keys(req.body).includes('type'))) {
-      return res.status(400).json(formatoResponse(400, 'Bad request', 'Campos erroneos', ''))
+    if (
+      !(
+        Object.keys(req.body).includes('_id') &&
+        Object.keys(req.body).includes('type')
+      )
+    ) {
+      return res
+        .status(400)
+        .json(formatoResponse(400, 'Bad request', 'Campos erroneos', ''));
     }
-    
+
     const { _id, type, ...data } = req.body;
-    
-    try {
-      result = await User.findById({ _id: req.body._id })
-      
-      if (!result)
-      return res.status(404).json(formatoResponse(404, 'User Not found ', 'usuario NO existe en la base de datos', ''));
 
-      
+    try {
+      result = await User.findById({ _id: req.body._id });
+
+      if (!result)
+        return res
+          .status(404)
+          .json(
+            formatoResponse(
+              404,
+              'User Not found ',
+              'usuario NO existe en la base de datos',
+              ''
+            )
+          );
     } catch (error) {
-      res.status(500).json(formatoResponse(500, '', 'Error DB', 'ERRORDB'))
+      res.status(500).json(formatoResponse(500, '', 'Error DB', 'ERRORDB'));
     }
 
-    
-    
     await sender({
       to: result.email,
       templateId: process.env[type],
       dynamicTemplateData: data,
     });
 
-    res.status(200).end()
-    
+    res.status(200).end();
   } catch (err) {
-    next(err)
+    next(err);
     console.log('error');
   }
 });
-
-
-
-
 
 module.exports = router;
